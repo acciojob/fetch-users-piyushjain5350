7377509c6b9c5943799d51fee64a61963bfa329b
Piyush Jain <piyushjain5350@gmail.com>
2023-08-31 21:30:13 +0530

    3 soln

--- src/components/Display.js
+++ src/components/Display.js
@@ -14,38 +14,66 @@ const Display=()=>{
     }
 
     return(
-        <div className="display">
-            <div className="heading-cc">
-                <p>Blue whales</p>
-                <button className="btn" type="submit" onClick={handlingFn}>Get User List</button>
-            </div>
-            <div>
-                <table>
-                    <thead>
-                        <th>First Name</th>
-                        <th>last Name</th>
-                        <th>Email</th>
-                        <th>Avatar</th>
-                    </thead>
-                    <tbody>
-                        {
-                            !fetchData&& <li>No data found to display.</li>
-                        }
-                        {
-                            fetchData&&fetchData.map(item=>(
-                                <tr key={item.id}>
-                                    <td>{item.first_name}</td>
-                                    <td>{item.last_name}</td>
-                                    <td>{item.email}</td>
-                                    <td><img src={item.avatar} alt="" /></td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
+        // <div className="display">
+        //     <div className="heading-cc">
+        //         <p>Blue whales</p>
+        //         <button className="btn" type="submit" onClick={handlingFn}>Get User List</button>
+        //     </div>
+        //     <div>
+        //         <table>
+        //             <thead>
+        //                 <th>First Name</th>
+        //                 <th>last Name</th>
+        //                 <th>Email</th>
+        //                 <th>Avatar</th>
+        //             </thead>
+        //             <tbody>
+        //                 {
+        //                     !fetchData&& <li>No data found to display.</li>
+        //                 }
+        //                 {
+        //                     fetchData&&fetchData.map(item=>(
+        //                         <tr key={item.id}>
+        //                             <td>{item.first_name}</td>
+        //                             <td>{item.last_name}</td>
+        //                             <td>{item.email}</td>
+        //                             <td><img src={item.avatar} alt="" /></td>
+        //                         </tr>
+        //                     ))
+        //                 }
+        //             </tbody>
+        //         </table>
+        //     </div>
 
-        </div>
+        // </div>
+        <div>
+        <button className='btn' onClick={handlingFn}>Get User List</button>
+        <table>
+            <thead>
+                <tr>
+                   <th>First Name</th> 
+                   <th>Last Name</th> 
+                   <th>Email</th> 
+                   <th>Avatar</th> 
+                </tr>
+            </thead>
+            <tbody>
+                {
+                (fetchData.length==0)? <p>No data found to display.</p> : fetchData.map(element => {
+                    return(
+                        <tr>
+                            <td>{element.first_name}</td>
+                            <td>{element.last_name}</td>
+                            <td>{element.email}</td>
+                            <td><img 
+                            src= {element.avatar}/></td>
+                        </tr>
+                    )
+                  })
+                }
+            </tbody>
+        </table>
+       </div>
     )
 }
 
